b345dd6cccc59123a5b90191e712343d7e5d639c
muhammadshiva <shivamuhammad07@gmail.com>
2021-11-16 12:44:00 +0700

    init project

---
+++ resources/views/index.blade.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <title>Molidu Learning</title>
+
+  <link rel="stylesheet" href="../assets/css/maicons.css">
+
+  <link rel="stylesheet" href="../assets/css/bootstrap.css">
+
+  <link rel="stylesheet" href="../assets/vendor/animate/animate.css">
+
+  <link rel="stylesheet" href="../assets/css/theme.css">
+
+</head>
+<body>
+
+  <!-- Back to top button -->
+  <div class="back-to-top"></div>
+
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-white sticky" data-offset="500">
+      <div class="container">
+        <a href="#" class="navbar-brand"><span class="text-primary">Molidu</span></a>
+
+        <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="navbar-collapse collapse" id="navbarContent">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="/">Beranda</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Tentang Kami</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Layanan</a>
+            </li>
+            {{-- <li class="nav-item">
+                <a class="nav-link" href="/news">News</a>
+              </li> --}}
+            <li class="nav-item">
+              <a class="btn btn-primary ml-lg-2" href="#">Subscriptions</a>
+            </li>
+          </ul>
+        </div>
+
+      </div>
+    </nav>
+
+    <div class="container">
+      <div class="page-banner home-banner">
+        <div class="row align-items-center flex-wrap-reverse h-100">
+          <div class="col-md-6 py-5 wow fadeInLeft">
+            <h1 class="mb-4">Welcome To Molidu</h1>
+            <p class="text-lg text-grey mb-5">Solusi Pembelajaran Daring Secara Sistematis, Edukatif dan Interaktif.”</p>
+            <a href="#" class="btn btn-primary btn-split" target="_blank">Watch Video <div class="fab"><span class="mai-play"></span></div></a>
+          </div>
+          <div class="col-md-6 py-5 wow zoomIn">
+            <div class="img-fluid text-center">
+              <img src="../assets/img/teaching.svg" alt="">
+            </div>
+          </div>
+        </div>
+        <a href="#about" class="btn-scroll" data-role="smoothscroll"><span class="mai-arrow-down"></span></a>
+      </div>
+    </div>
+  </header>
+
+  <div class="page-section" id="about">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-lg-6 py-3 wow fadeInUp">
+          <span class="subhead">Tentang Kami</span>
+          <h2 class="title-section">Mobile Learning And Education</h2>
+          <div class="divider"></div>
+          <p>Molidu merupakan sebuah platform pembelajaran online yang menyediakan solusi untuk guru dan murid dalam proses kegiatan belajar mengajar secara efektif, efisien, dan terstruktur.</p>
+          <p>melalui platform Molidu, kegiatan pembelajaran secara daring seperti absensi, pengerjaan tugas oleh siswa, pemberian materi oleh guru dapat lebih mudah dilakukan. </p>
+          <a href="about.html" class="btn btn-primary mt-3">Read More</a>
+        </div>
+        <div class="col-lg-6 py-3 wow fadeInRight">
+          <div class="img-fluid py-3 text-center">
+            <img src="../assets/img/developer.png" width="500" height="500" alt="">
+          </div>
+        </div>
+      </div>
+    </div> <!-- .container -->
+  </div> <!-- .page-section -->
+
+  <!-- Banner info -->
+  <div class="page-section banner-info">
+    <div class="wrap bg-image" style="background-image: url(../assets/img/bg_pattern.svg);">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-lg-6 py-3 pr-lg-5 wow fadeInUp">
+            <h2 class="title-section">Bagaimana Molidu Dapat <br> Meningkatkan Efisiensi Belajar ?</h2>
+            <div class="divider"></div>
+            <p>We're an experienced and talented team of passionate consultants who breathe with search engine marketing.</p>
+
+            <ul class="theme-list theme-list-light text-white">
+              <li>
+                <div class="h5">Lorem</div>
+                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor</p>
+              </li>
+              <li>
+                <div class="h5">Lorem</div>
+                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor</p>
+              </li>
+            </ul>
+          </div>
+          <div class="col-lg-6 py-3 wow fadeInRight">
+            <div class="img-fluid text-center">
+              <img src="../assets/img/banner_image_2.svg" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div> <!-- .wrap -->
+  </div> <!-- .page-section -->
+
+  <div class="page-section">
+    <div class="container">
+      <div class="text-center wow fadeInUp">
+        <div class="subhead">Yuk Berlangganan !</div>
+        {{-- <h2 class="title-section">Choose plan the right for you</h2> --}}
+        <div class="divider mx-auto"></div>
+      </div>
+      <div class="row mt-5">
+
+        <div class="col-lg-4 py-3 wow zoomIn">
+
+        </div>
+
+
+        <div class="col-lg-4 py-3 wow zoomIn">
+            <div class="card-pricing marked">
+              <div class="header">
+                <div class="pricing-type">Start From</div>
+                <div class="price">
+                  <span class="dollar">$</span>
+                  <h1>19<span class="suffix">.99</span></h1>
+                </div>
+                <h5>Per Month</h5>
+              </div>
+              <div class="body">
+                <p>25 Analytics <span class="suffix">Campaign</span></p>
+                <p>1,300 Change <span class="suffix">Keywords</span></p>
+                <p>Social Media <span class="suffix">Reviews</span></p>
+                <p>1 Free <span class="suffix">Optimization</span></p>
+                <p>24/7 <span class="suffix">Support</span></p>
+              </div>
+              <div class="footer">
+                <a href="#" class="btn btn-pricing btn-block">Subscribe</a>
+              </div>
+            </div>
+          </div>
+
+
+
+          <div class="col-lg-4 py-3 wow zoomIn">
+
+          </div>
+
+      </div>
+    </div> <!-- .container -->
+  </div> <!-- .page-section -->
+
+
+  <footer class="page-footer bg-image" style="background-image: url(../assets/img/world_pattern.svg);">
+    <div class="container">
+      <div class="row mb-5">
+        <div class="col-lg-3 py-3">
+          <h3>Molidu</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero amet, repellendus eius blanditiis in iusto eligendi iure.</p>
+
+          <div class="social-media-button">
+            <a href="#"><span class="mai-logo-facebook-f"></span></a>
+            <a href="#"><span class="mai-logo-twitter"></span></a>
+            <a href="#"><span class="mai-logo-google-plus-g"></span></a>
+            <a href="#"><span class="mai-logo-instagram"></span></a>
+            <a href="#"><span class="mai-logo-youtube"></span></a>
+          </div>
+        </div>
+        <div class="col-lg-3 py-3">
+          <h5>Company</h5>
+          <ul class="footer-menu">
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Career</a></li>
+            <li><a href="#">Advertise</a></li>
+            <li><a href="#">Terms of Service</a></li>
+            <li><a href="#">Help & Support</a></li>
+          </ul>
+        </div>
+        <div class="col-lg-3 py-3">
+          <h5>Kontak Kami</h5>
+          <p>Politeknik Negeri Malang</p>
+          <a href="#" class="footer-link">+00 1122 3344 5566</a>
+          <a href="#" class="footer-link">molidulearning@gmail.com</a>
+        </div>
+        <div class="col-lg-3 py-3">
+          <h5>Newsletter</h5>
+          <p>Dapatkan pemberitahuan terupdate dari Molidu</p>
+          <form action="#">
+            <input type="text" class="form-control" placeholder="Enter your email..">
+            <button type="submit" class="btn btn-success btn-block mt-2">Subscribe</button>
+          </form>
+        </div>
+      </div>
+
+      <p class="text-center" id="copyright">Copyright &copy; 2021. Design and develop by <a href="https://macodeid.com/" target="_blank">Molidu Team</a></p>
+    </div>
+  </footer>
+
+<script src="../assets/js/jquery-3.5.1.min.js"></script>
+
+<script src="../assets/js/bootstrap.bundle.min.js"></script>
+
+<script src="../assets/js/google-maps.js"></script>
+
+<script src="../assets/vendor/wow/wow.min.js"></script>
+
+<script src="../assets/js/theme.js"></script>
+
+</body>
+</html>
